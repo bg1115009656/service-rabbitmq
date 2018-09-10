51f967cc83cb49e318ff8423379b75f97fc25948
baigang <baigang1995@gmail.com>
2018-09-10 10:46:44 +0800

    rabittmq信任的包配置

---
+++ rabbitmqpackages/src/main/java/com/gbai/RabbitmqpackagesApplication.java
@@ -0,0 +1,12 @@
+package com.gbai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqpackagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqpackagesApplication.class, args);
+    }
+}
